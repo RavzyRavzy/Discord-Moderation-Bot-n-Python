5be5eb8f266e1d689370e0a600dd98267553375a
Ravzy.dev <Devrimdrn123@icloud.com>
2026-01-28 17:38:52 +0100

    start.bat

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+title Moderasyon Botu
+echo   ____                       _     _                   
+echo  |  _ \ __ ___   _______   _| |__ (_)_____ __ ___ 
+echo | |_) / _` \ \ / /_  / | | | '_ \| |_  / '_ ` _ \  
+echo |  _ < (_| |\ V / / /| |_| | | | | |/ /| | | | | |
+echo |_| \_\__,_| \_/ /___|\__, |_| |_|_/___|_| |_| |_|
+echo                                        |___/
+echo Bot başlatılıyor...
+python main.py
+pause
